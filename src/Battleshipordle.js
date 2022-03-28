44dac11625d343e542a38f95cee744eb432b8abf
TTS4life <TTStudios4life@gmail.com>
2022-03-28 18:42:57 -0500

    Implement inputting characters into cells, use tab/enter to move between cells

--- src/Battleshipordle.js
+++ src/Battleshipordle.js
@@ -1,9 +1,17 @@
 export const Battleshipordle = {
-    setup: () => ({ cells: Array(100).fill(null) }),
-  
-    moves: {
-      clickCell: (G, ctx, id) => {
-        G.cells[id] = ctx.currentPlayer;
-      },
+  setup: () => ({ cells: Array(100).fill(null) }),
+
+  moves: {
+    clickCell: (G, ctx, id) => {
+      G.cells[id] = ctx.currentPlayer;
     },
-  };
+    
+    insertLetter: (G, ctx, id, letter) => {
+      G.cells[id] = letter;
+    },
+
+    clearLetter: (G, ctx, id) => {
+      G.cells[id] = null;
+    }
+  },
+};
