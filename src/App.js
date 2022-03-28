f476e5ac0beab5ebd5c45eb846ce91cbb2083a5d
Trevor Houck <houck058@d.umn.edu>
2022-03-28 13:39:17 -0500

    Add board template (not building)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Client } from 'boardgame.io/react';
 import { Battleshipordle } from './Battleshipordle'
+import { BattleshipordleBoard } from './BattleshipordleBoard';
 
 
-const App = Client({ game: Battleshipordle });
+const App = Client({ 
+  game: Battleshipordle,
+  board: BattleshipordleBoard
+});
 
 export default App;
