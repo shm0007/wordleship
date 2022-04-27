ac125e7e6d33281520a6ae7465a11c10ff9164f9
TTS4life <TTStudios4life@gmail.com>
2022-04-26 21:16:04 -0500

    Add player selection screen for multiplayer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import React from 'react';
 import { Client } from 'boardgame.io/react';
 import { Battleshipordle } from './Battleshipordle';
 import Board from './BattleshipordleBoard';
 import { SocketIO } from 'boardgame.io/multiplayer'
 
-const App = Client({
+const BattleshipordleClient = Client({
   game: Battleshipordle,
   board: Board,
-  //multiplayer: SocketIO({ server: 'localhost:8000' }), // Needed for multiplayer
+  multiplayer: SocketIO({ server: 'localhost:8000' }), // Needed for multiplayer
+  // debug: false,
 });
+
+class App extends React.Component {
+  state = { playerID: null };
+
+  render() {
+    if(this.state.playerID === null) {
+      return (
+        <div>
+          <p>Play as</p>
+          <button onClick={() => this.setState({ playerID: "0" })}>
+            Player 0
+          </button>
+          <button onClick={() => this.setState({ playerID: "1" })}>
+            Player 1
+          </button>
+        </div>
+      )
+    }
+    
+    return (
+      <div>
+        <BattleshipordleClient playerID={this.state.playerID} />
+      </div>
+    );
+  }
+}
 
 export default App;
