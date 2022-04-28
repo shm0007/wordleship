94657064bd06a90325351a07ccd8aa41c4b9ced6
ehsan011 <ehsan011@d.umn.edu>
2022-04-28 04:38:24 -0500

    complete findMatch function to check status after each attack

--- src/Battleshipordle.js
+++ src/Battleshipordle.js
@@ -222,53 +222,72 @@ function placeShip(G, ctx, id) {
   return word;
 }
 /**
- * Find Complete/Partial/zero match for each letters of an Attack word. - not finished, WIP
+ * Find Complete/Partial/zero match for each letters of an Attack word. 
  * 
  * @param {object} G 
  * @param {object} ctx 
  * @param {object} wordObj - wordObj with letter and coordinates
  * @returns object - modified version of wordObj with additional attribute stat which could either be C/P/Z
  */
-function findMatch(G, ctx,wordObj) {
+ function findMatch(G, ctx,wordObj) {
   let coord = 0;
   let letter = '';
   let enemy = getOtherPlayer(ctx.currentPlayer);
-  
+  console.log("AttackObj" + wordObj);
   let word = [];
   for(let i = 0; i < wordObj.length; i++) {
     
-    if(G.oldBoardState[enemy][wordObj[i]['coord']] !== null ) {
+     
       letter = wordObj[i]['letter'];
       coord = wordObj[i]['coord'];
-      if(G.oldBoardState[enemy][coord] === wordObj[i]['coord']){
+      if( G.oldBoardState[enemy][coord] !== null && G.oldBoardState[enemy][coord] === wordObj[i]['coord']){
         word.push({'letter': letter, 'coord': wordObj[i]['coord'], stat: "C"});
       }else{
           let letterFound = "Z";
           for(let j = coord+1; j< BOARD_SIZE &&  j< coord+ (10- coord%10); j++){  // search in right to find letter
-              if(G.oldBoardState[enemy][coord] === null)
+            if(G.oldBoardState[enemy][coord] === null)
                 break;
-              if(G.oldBoardState[enemy][coord] === letter){
+                //console.log("j is "+ j + " G.oldBoardState[enemy][coord] "+ G.oldBoardState[enemy][coord] + " letter "+ letter);  
+
+              if(G.oldBoardState[enemy][j] === letter){
                   letterFound = "P";
                   break; 
               }
           }
           for(let j = coord-1; j>= coord/10; j--){ // search in left to find letter
-            if(G.oldBoardState[enemy][coord] === null)
+            if(G.oldBoardState[enemy][j] === null)
               break;
-            if(G.oldBoardState[enemy][coord] === letter){
+            if(G.oldBoardState[enemy][j] === letter){
                 letterFound = "P";
                 break; 
             }
-        }
+          }
+          for(let j = coord+10; j< BOARD_SIZE; j+=10){ // search in down to find letter
+            if(G.oldBoardState[enemy][j] === null)
+              break;
+            if(G.oldBoardState[enemy][j] === letter){
+                letterFound = "P";
+                break; 
+            }
+          }
+          for(let j = coord-10; j >=0; j-=10){ // search in up to find letter
+            if(G.oldBoardState[enemy][j] === null)
+              break;
+            if(G.oldBoardState[enemy][j] === letter){
+                letterFound = "P";
+                break; 
+            }
+          }
+          
+          
         //TODO: Need to add two more loops to find match in a vertical word
         word.push({'letter': letter, 'coord': coord, stat: letterFound});
       }
-     }
+     
   }
   console.log(word);
   return word;
 }
-
 
 /**
  * Reset the board state to the beginning of the current player's
