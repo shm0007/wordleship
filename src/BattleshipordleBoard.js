6382ea8a2ec098162ca29565f742bd70adda2bb5
TTS4life <TTStudios4life@gmail.com>
2022-04-28 13:29:25 -0500

    Update UI design to be cleaner

--- src/BattleshipordleBoard.js
+++ src/BattleshipordleBoard.js
@@ -113,6 +113,23 @@ class Board extends React.Component {
             You are Player {this.props.playerID ? this.props.playerID : 'N/A'}
           </div>
         </div>
+        <h1 className="turn" Style="text-align: center">
+          It is Player {this.props.ctx.currentPlayer}'s turn
+        </h1>
+        <h2 className="instruction" Style="text-align: center">
+          {this.props.G.current_instruction}
+        </h2>
+        <div class='table-wrapper'>
+          <div className="player-board-title">
+            Player {this.boardToRender()}'s board
+          </div>
+          <table id="board">
+            <tbody>{this.renderBoard()}</tbody>
+          </table>
+          <div className="player">
+            You are Player {this.props.playerID ? this.props.playerID : 'N/A'}
+          </div>
+        </div>
         {winner}
         <div className="button-options">
           {this.props.ctx.phase ==="setup" ? placeShipButton : ''}
