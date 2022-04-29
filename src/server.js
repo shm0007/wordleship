9348fe4ed4c36a8f64770c68bf0e58d2ad30a1f3
TTS4life <TTStudios4life@gmail.com>
2022-04-28 22:36:53 -0500

    Remove homepage for heroku deployment, add local build and unbuilt testing and re-implement server for local testing

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const { Server, Origins } = require('boardgame.io/server');
+const { Battleshipordle } = require('./Battleshipordle');
+
+const server = Server({
+  games: [Battleshipordle],
+  origins: [
+    Origins.LOCALHOST,
+  ]
+});
+
+server.run(8000);
