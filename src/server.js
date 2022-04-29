d5590b5a18ef91d1b9b88bb0b86bea9ed85764a0
TTS4life <TTStudios4life@gmail.com>
2022-04-28 21:59:54 -0500

    Modify scripts for Heroku deployment

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
 const { Server, Origins } = require('boardgame.io/server');
+import path from 'path';
+import serve from 'koa-static';
 const { Battleshipordle } = require('./Battleshipordle');
 
-const server = Server({
-  games: [Battleshipordle],
-  origins: [
-    "192.168.1.151",
-    Origins.LOCALHOST,
-  ]
-});
+const server = Server({ games: [Battleshipordle] });
+const PORT = process.env.PORT || 8000;
 
-server.run(8000);
+//buld path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath));
+
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath) (
+      Object.assign(ctx, {path: 'index.html'}),
+      next
+    )
+  )
+});
